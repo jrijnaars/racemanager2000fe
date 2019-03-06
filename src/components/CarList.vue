0aa746160ebacd28dfaf9cf8c522e1b752266c4a
jrijnaars <jrijnaars@gmail.com>
2019-03-06 07:44:10 +0100

    added bootstrap for styling

--- src/components/CarList.vue
+++ src/components/CarList.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
-    <car v-for="car in cars" :key="car.id" v-bind:car="car"></car>
+    <div>
+      <h2 class="h2">Drivers</h2>
+      <car v-for="car in cars" :key="car.id" v-bind:car="car"></car>
+    </div>
+    <b-button class="button">New driver</b-button>
   </div>
 </template>
 
 <script>
+
   import Car from "@/components/Car";
 
   export default {
+    components: {Car},
     props: ['cars'],
     name: "CarList",
-    components: {
-      Car,
-    },
-  };
+  }
 </script>
 
 <style>
+h2 {
+  width: 100%;
+  padding: 20px;
+  color: #212529;
+  background-color: #ECECEC;
+  text-align: center;
+}
 
+button {
+  margin-left: 10px;
+  }
 </style>
---
+++ src/components/CreateCar.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "CreateCar"
+    }
+</script>
+
+<style scoped>
+
+</style>
